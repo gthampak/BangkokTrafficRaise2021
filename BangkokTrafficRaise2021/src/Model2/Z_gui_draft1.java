b4999f84fdecfda392b9939633b91a077d490a0f
Guy_T <gthampak@gmail.com>
2021-07-06 20:39:10 +0700

    GUI progress

--- BangkokTrafficRaise2021/src/Model2/Z_gui_draft1.java
+++ BangkokTrafficRaise2021/src/Model2/Z_gui_draft1.java
@@ -7,14 +7,17 @@ import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JTextField;
 
 public class Z_gui_draft1 {
 
 	private JFrame frame;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -32,6 +35,8 @@ public class Z_gui_draft1 {
 		});
 	}
 
+	
+	
 	/**
 	 * Create the application.
 	 */
@@ -49,12 +54,18 @@ public class Z_gui_draft1 {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
+		ArrayList<ArrayList<JComponent>> components = new ArrayList<>();
+		
 		ArrayList<JTextArea> tas = new ArrayList<>();
 		ArrayList<JComboBox> cbs = new ArrayList<>();
 		ArrayList<JScrollPane> sps = new ArrayList<>();
 		
+		//RoadNetwork Stuff
+		
 		Main main = new Main();
 		RoadNetwork RN = main.caseStudy1();
+		
+		//RoadNetwork Stuff End
 		
 		int cbx = 210;
 		int cby = 20;
@@ -105,6 +116,7 @@ public class Z_gui_draft1 {
 		}
 		
 		JButton btnNewButton = new JButton();
+		btnNewButton.setText("");
 		btnNewButton.addMouseListener(new MouseAdapter() {
 			public void mouseReleased(MouseEvent e) {
 				
@@ -128,6 +140,72 @@ public class Z_gui_draft1 {
 		btnNewButton.setBounds(210*6, 0, 20, 20);
 		frame.getContentPane().add(btnNewButton);
 		
+		textField = new JTextField();
+		textField.setBounds(210*6, 40, 20, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
 		
+		int counter = 0;
+		
+		for(Road r: RN.allRoads()) {
+			cbs.get(counter).setSelectedItem(r);
+			counter++;
+		}
+		
+		JButton btnNewButton_1 = new JButton("");
+		btnNewButton_1.setBounds(210*6, 60, 20, 20);
+		btnNewButton_1.addMouseListener(new MouseAdapter() {
+			public void mouseReleased(MouseEvent e) {
+				
+				for(int j = 0; j < Integer.parseInt(textField.getText()); j++) {
+					
+					pause();
+					RN.iterateWSpeed();
+					
+					for(int i = 0; i < tas.size(); i++) {
+						
+						JComboBox cb = cbs.get(i);
+						JTextArea ta = tas.get(i);
+						JScrollPane sp = sps.get(i);
+						
+						if(cb.getSelectedItem() != null) {
+							Road r = (Road) cb.getSelectedItem();
+							ta.setText(r.printRoad1());
+						}
+						
+						frame.getContentPane().add(cb);
+						frame.getContentPane().add(sp);
+					}
+					
+					newCars(RN);
+					
+				}
+				
+			}
+		});
+		frame.getContentPane().add(btnNewButton_1);
 	}
+	
+	/**
+	 * 0.5 second pause
+	 */
+	public static void pause() {
+		try {
+			Thread.sleep(500);
+		} catch (Exception e) {
+		}
+	}
+	
+	public void newCars(RoadNetwork RN) {
+		if(RN.iterations()%3 == 0) {
+			for(int i = 6; i < 16; i++) {
+				
+				for(int j = 0; j < RN.roadsFrom(i).get(0).lanes().length; j++) {
+					RN.roadsFrom(i).get(0).lanes()[j].insertCar(new Car(RN.iterations()));
+				}
+				
+			}
+		}
+	}
+	
 }
